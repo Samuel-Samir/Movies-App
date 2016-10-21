9216d025e2d6523d36d195290cde65d8ffd9ce79
Samuel-Samir <samuelsamer952@gmail.com>
2016-10-21 14:20:36 +0200

    Add PopularFragment

---
+++ app/src/main/java/com/example/android/movies_app/PopularFragment.java
@@ -0,0 +1,21 @@
+package com.example.android.movies_app;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PopularFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_popular, container, false);
+    }
+
+}
